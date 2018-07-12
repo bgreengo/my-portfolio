c2bf5d32278eb1a833838e031c6944fb947c799d
Blake Green <green@Blakes-MacBook-Pro.local>
2018-07-12 08:33:08 -0700

    make examples dynamic

--- js/main.js
+++ js/main.js
@@ -1,5 +1,32 @@
-import 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ExampleWork from './example-work';
 
-console.log("Loaded react-dom");
+const myWork = [
+    {
+        'title': "Serverless",
+        'image': {
+            'desc': "Serverless work examples",
+            'src': "images/example1.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Containers",
+        'image': {
+            'desc': "Container work examples",
+            'src': "images/example2.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "IaC",
+        'image': {
+            'desc': "Infrastructure as code work examples",
+            'src': "images/example3.png",
+            'comment': ""
+        }
+    }
+]
 
-console.log("Webpack works");
+ReactDOM.render(<ExampleWork work={myWork}/>, document.getElementById('example-work'));
