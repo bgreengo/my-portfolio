62b5be37efbf4d577377014b0ecbeda5ebf62240
Blake Green <green@Blakes-MacBook-Pro.local>
2018-07-12 12:43:37 -0700

    added more bubbles

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# my-portfolio
-my serverless portfolio
+# Serverless Portfolio
+
+
--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ import ExampleWork from './example-work';
 const myWork = [
     {
         'title': "Serverless",
-        'href': "https://example.com",
-        'desc': "Aliqua anim reprehenderit ipsum laborum ea veniam incididunt.",
+        'href': "https://github.com/bgreengo/serverless-portfolio",
+        'desc': "This is just the source code for my first serverless application but I expect to update this near future.",
         'image': {
             'desc': "Serverless work examples",
             'src': "images/example1.png",
@@ -15,8 +15,8 @@ const myWork = [
     },
     {
         'title': "Containers",
-        'href': "https://example.com",
-        'desc': "Aliqua anim reprehenderit ipsum laborum ea veniam incididunt.",
+        'href': "https://github.com/bgreengo/docker-elk",
+        'desc': "An easy way to setup an ELK stack using Docker. I plan to fill this with more emphasis on Kubernetes.",
         'image': {
             'desc': "Container work examples",
             'src': "images/example2.png",
@@ -25,11 +25,31 @@ const myWork = [
     },
     {
         'title': "IaC",
-        'href': "https://example.com",
-        'desc': "Aliqua anim reprehenderit ipsum laborum ea veniam incididunt.",
+        'href': "https://github.com/bgreengo/aws-dr",
+        'desc': "In this example, I used AWS CloudFormation to deploy a disaster recovery scenario. This deployment uses Route 53 health checks to send an SNS notification that will trigger a Lambda function to start an AMI in a different region automatically.",
         'image': {
             'desc': "Infrastructure as code work examples",
             'src': "images/example3.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "CI/CD",
+        'href': "https://github.com/bgreengo/wordpress-aws-terraform-automation",
+        'desc': "Using Terraform, I automated a CI/CD pipeline with AWS CodeCommit, CodePipeline, Elastic Beanstalk, IAM, EFS, and RDS.",
+        'image': {
+            'desc': "CI/CD work examples",
+            'src': "images/example4.png",
+            'comment': ""
+        }
+    },
+    {
+        'title': "Config Mgmt",
+        'href': "https://github.com/bgreengo/ansible-examples",
+        'desc': "Plan is to update this once I have more config management examples using Ansible or Chef.",
+        'image': {
+            'desc': "Config Mgmt work examples",
+            'src': "images/example5.png",
             'comment': ""
         }
     }
